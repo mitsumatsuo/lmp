922391fcf4f2e3f983708e094c086476095ca26c
Mitsu Matsuo <75003091+mitsumatsuo@users.noreply.github.com>
2022-02-11 08:46:47 +0900

    fix little

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-  secret: process.env.NEXT_PUBLIC_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
